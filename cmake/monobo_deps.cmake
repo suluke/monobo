d8220722988f2e7d52d273b915b9ef456a22e8a1
Lukas Böhm <suluke93@gmail.com>
2021-12-05 18:09:21 +0100

    Try fixing cmake build

--- cmake/monobo_deps.cmake
+++ cmake/monobo_deps.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        master
+  GIT_TAG        main
 )
 FetchContent_Populate(googletest)
 add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
