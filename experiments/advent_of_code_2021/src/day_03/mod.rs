14b5da7c79ccd6d41a810c3d52254e45ab13f472
Lukas Böhm <suluke93@gmail.com>
2021-12-06 21:47:10 +0100

    [Advent] day3/part2

--- experiments/advent_of_code_2021/src/day_03/mod.rs
+++ experiments/advent_of_code_2021/src/day_03/mod.rs
@@ -42,6 +42,69 @@ fn part1() -> std::io::Result<()> {
     Ok(())
 }
 
+fn oxgen(lines: &[String]) -> usize {
+    let mut rem = lines.to_vec();
+    let mut pos = 0;
+    while rem.len() > 1 {
+        let mc = if rem.iter().fold(0, |acc, val| {
+            acc + (val.chars().nth(pos).unwrap() == '1') as usize
+        }) * 2 >= rem.len() {'1'} else {'0'};
+        rem = rem.iter().filter(|str| {str.chars().nth(pos).unwrap() == mc}).cloned().collect();
+        pos += 1;
+    }
+    rem[0]
+        .chars()
+        .fold(0, |acc, bit| acc * 2 + (bit == '1') as usize)
+}
+fn coscrub(lines: &[String]) -> usize {
+    let mut rem = lines.to_vec();
+    let mut pos = 0;
+    while rem.len() > 1 {
+        let mc = if rem.iter().fold(0, |acc, val| {
+            acc + (val.chars().nth(pos).unwrap() == '0') as usize
+        }) * 2 <= rem.len() {'0'} else {'1'};
+        rem = rem.iter().filter(|str| {str.chars().nth(pos).unwrap() == mc}).cloned().collect();
+        pos += 1;
+    }
+    rem[0]
+        .chars()
+        .fold(0, |acc, bit| acc * 2 + (bit == '1') as usize)
+}
+
+fn part2() -> std::io::Result<()> {
+    let mut input_path = PathBuf::new();
+    input_path.push(env!("CARGO_MANIFEST_DIR"));
+    input_path.push("input");
+    input_path.push("03.txt");
+    let lines: Vec<String> = fs::read_to_string(input_path)
+        .unwrap()
+        .lines()
+        .map(str::to_owned)
+        .collect();
+    let ox = oxgen(&lines);
+    let co = coscrub(&lines);
+    println!("day3/pt2: {}", ox * co);
+    Ok(())
+}
+
 pub fn solve() -> std::io::Result<()> {
-    part1()
+    part1()?;
+    part2()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn pt2() {
+        let lines: Vec<String> =
+            "00100\n11110\n10110\n10111\n10101\n01111\n00111\n11100\n10000\n11001\n00010\n01010"
+                .lines()
+                .map(str::to_owned)
+                .collect();
+        let ox = oxgen(&lines);
+        let co = coscrub(&lines);
+        assert_eq!(ox, 23);
+        assert_eq!(co, 10);
+    }
 }
